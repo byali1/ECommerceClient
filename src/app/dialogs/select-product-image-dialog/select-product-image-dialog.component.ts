e2c400fdc873309b884f91d1b0c433f517dec6d1
byali <ali_ilgin@outlook.com>
2023-10-04 19:40:38 +0300

    #33 Ürünlerin Resmini Silme

--- src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
+++ src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
@@ -1,19 +1,66 @@
-import { Component, Inject, Output } from '@angular/core';
+import { Component, Inject, OnInit, Output } from '@angular/core';
 import { BaseDialog } from '../base/base-dialog';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FileUploadOptions } from 'src/app/services/common/file-upload/file-upload.component';
+import { ListProductImage } from 'src/app/contracts/list_product_image';
+import { ProductService } from 'src/app/services/common/models/product.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
+import { MatCard } from '@angular/material/card';
+import { DialogService } from 'src/app/services/common/dialog.service';
+import {
+  DeleteDialogComponent,
+  DeleteState,
+} from '../delete-dialog/delete-dialog.component';
+import { async } from 'rxjs';
+
+declare var $: any;
 
 @Component({
   selector: 'app-select-product-image-dialog',
   templateUrl: './select-product-image-dialog.component.html',
   styleUrls: ['./select-product-image-dialog.component.scss'],
 })
-export class SelectProductImageDialogComponent extends BaseDialog<SelectProductImageDialogComponent> {
+export class SelectProductImageDialogComponent
+  extends BaseDialog<SelectProductImageDialogComponent>
+  implements OnInit
+{
   constructor(
     dialogRef: MatDialogRef<SelectProductImageDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: SelectProductImageState | string
+    @Inject(MAT_DIALOG_DATA) public data: SelectProductImageState | string,
+    private productService: ProductService,
+    private spinner: NgxSpinnerService,
+    private dialogService: DialogService
   ) {
     super(dialogRef);
+  }
+
+  images: ListProductImage[];
+
+  async ngOnInit() {
+    this.spinner.show(SpinnerType.BallSpinClockWise);
+    this.images = await this.productService.readImages(
+      this.data as string,
+      () => this.spinner.hide(SpinnerType.BallSpinClockWise)
+    );
+  }
+
+  async deleteProductImage(imageId: string, event: any) {
+    this.dialogService.openDialog({
+      componentType: DeleteDialogComponent,
+      data: DeleteState.Yes,
+      afterClosed: async () => {
+        this.spinner.show(SpinnerType.Cog);
+        const card = $(event.target).closest('.product-image-card');
+        card.fadeOut(900, async () => {
+          await this.productService.deleteProductImage(this.data as string, imageId, () => {
+            card.remove();
+          this.spinner.hide(SpinnerType.Cog);
+
+        });
+      })
+    }
+    });
   }
 
   @Output() options: Partial<FileUploadOptions> = {
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -4,6 +4,7 @@ import { Create_Product } from 'src/app/contracts/create_product';
 import { HttpErrorResponse } from '@angular/common/http';
 import { List_Product } from 'src/app/contracts/list_product';
 import { Observable, firstValueFrom } from 'rxjs';
+import { ListProductImage } from 'src/app/contracts/list_product_image';
 
 @Injectable({
   providedIn: 'root',
@@ -42,9 +43,15 @@ export class ProductService {
     size: number = 5,
     successCallBack?: () => void,
     errorCallBack?: (errorMessage: string) => void
-  ): Promise<{totalCount:number;products:List_Product[]}> {
-    const promiseData: Promise<{totalCount:number;products:List_Product[]}> = firstValueFrom(
-      this.httpClientService.get<{totalCount:number;products:List_Product[]}>({
+  ): Promise<{ totalCount: number; products: List_Product[] }> {
+    const promiseData: Promise<{
+      totalCount: number;
+      products: List_Product[];
+    }> = firstValueFrom(
+      this.httpClientService.get<{
+        totalCount: number;
+        products: List_Product[];
+      }>({
         controller: 'products',
         queryString: `page=${page}&size=${size}`,
       })
@@ -58,12 +65,46 @@ export class ProductService {
     return await promiseData;
   }
 
-   async deleteProduct(id:string){
-   const deleteObservable: Observable<any> = this.httpClientService.delete<any>({
-      controller:"products"
-    },id);
+  async deleteProduct(id: string) {
+    const deleteObservable: Observable<any> =
+      this.httpClientService.delete<any>(
+        {
+          controller: 'products',
+        },
+        id
+      );
 
     await firstValueFrom(deleteObservable);
+  }
 
+  async readImages(
+    id: string,
+    successCallBack?: () => void
+  ): Promise<ListProductImage[]> {
+    const getObservable: Observable<ListProductImage[]> =
+      this.httpClientService.get<ListProductImage[]>(
+        {
+          action: 'GetProductImagesById',
+          controller: 'products',
+        },
+        id
+      );
+
+    const images: ListProductImage[] = await firstValueFrom(getObservable);
+    successCallBack();
+    return images;
+  }
+
+  async deleteProductImage(id: string, imageId: string,successCallBack?: () => void) {
+    const deleteObservable = this.httpClientService.delete(
+      {
+        action: 'DeleteProductImage',
+        controller: 'products',
+        queryString: `imageId=${imageId}`,
+      },
+      id
+    );
+    await firstValueFrom(deleteObservable);
+    successCallBack();
   }
 }
